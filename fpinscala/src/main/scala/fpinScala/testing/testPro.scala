e1acc452596a74103c8db3313d966a57f9ef6814
Tomcruseal <lzb731@hotmail.com>
2017-11-27 17:52:01 +0800

    refactoring ~

--- fpinscala/src/main/scala/fpinScala/testing/testPro.scala
+++ fpinscala/src/main/scala/fpinScala/testing/testPro.scala
@@ -42,4 +42,30 @@ def booleanGen: Gen[Boolean] = {
     Gen(State(RNG.boolean))    //?印象里并没有这个额
 }
 
-def listofN[A](n: Int, g: Gen[A]): Gen[List[A]]
+def listofN[A](n: Int, g: Gen[A]): Gen[List[A]] = {
+    Gen(State(RNG.ints(n)))
+}
+//答案是调用的sequence
+
+def listofN2[A](n: Int, g: Gen[A]): Gen[List[A]] = {
+    Gen(State.sequence(list.fill(n)(g.sample)))
+}
+//emmmmm
+
+def flatMap[B](f: A => Gen[B]): Gen[B] = {
+    Gen(sample.flatMap(a =>f(a).sample))
+}//是否有更简洁的写法？
+
+def listofN3[A](size: Int): Gen[List[A]] = {
+    Gen.listofN(size,this)
+}// it is just a method alias(I think it seems like a trick)
+
+def listofN4[A](size: Gen[Int]): Gen[List[A]] = {
+    size flatMap(a => listofN3(a))
+} //Here exists a tuny bug, think about it
+
+def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = {
+    if isOdd g1.sample.nextInt
+        Gen(g1.sample)
+    else Gen(g2.sample)
+}
