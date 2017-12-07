69d4540a9defffc5cda151d248000dedd2541ef3
Tomcruseal <lzb731@hotmail.com>
2017-12-07 20:06:22 +0800

    errors handling accomplished

--- fpinscala/src/main/scala/fpinScala/fpErrors/Eithers.scala
+++ fpinscala/src/main/scala/fpinScala/fpErrors/Eithers.scala
@@ -19,3 +19,16 @@ def Try(a: => A): Either[Exception, A]  = {
     try Right(a)
     catch {case e: Exception => Left(e)}
 }
+
+//下面代码直接复制于答案，因为懒得写了
+def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = 
+  es match {
+    case Nil => Right(Nil)
+    case h::t => (f(h) map2 traverse(t)(f))(_ :: _)
+  }
+
+def traverse_1[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = 
+  es.foldRight[Either[E,List[B]]](Right(Nil))((a, b) => f(a).map2(b)(_ :: _))
+
+def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = 
+  traverse(es)(x => x)
